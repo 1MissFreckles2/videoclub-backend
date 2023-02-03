1e275bb30157bfab14c2279f0eac77aacc1a8d07
PabloRomeroOrtega <promeroortega@gmail.com>
2023-02-03 10:09:27 +0100

    FileController.getByCodMovie method creation

--- src/main/java/com/videoclub/backend/controller/FileController.java
+++ src/main/java/com/videoclub/backend/controller/FileController.java
@@ -1,12 +1,15 @@
 package com.videoclub.backend.controller;
 
 import com.videoclub.backend.data.entity.FileEntity;
+import com.videoclub.backend.data.entity.MovieEntity;
 import com.videoclub.backend.data.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class FileController {
@@ -19,5 +22,10 @@ public class FileController {
         return fileRepository.findAll();
     }
 
+    @GetMapping("/video-club/file") // metodo que me trae por id el gender
+    public Optional<List<FileEntity>> getByCodMovie(@RequestParam Integer codMovie)
+    {
+        return Optional.of(fileRepository.findByCodMovie(codMovie)) ;
+    }
 }
 
--- src/main/java/com/videoclub/backend/data/repository/FileRepository.java
+++ src/main/java/com/videoclub/backend/data/repository/FileRepository.java
@@ -1,9 +1,14 @@
 package com.videoclub.backend.data.repository;
 
 import com.videoclub.backend.data.entity.FileEntity;
+import com.videoclub.backend.data.entity.MovieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FileRepository extends JpaRepository<FileEntity, Integer> {
+
+    List<FileEntity> findByCodMovie(Integer codMovie);
 }
